f0a21956f5ca0c1e012da6e541214d2ea8df430a
Ezechindu Chibuike <cezechindu@gmail.com>
2023-04-09 23:48:27 +0100

    Chitech

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=895284fb2d2c50a520ea537456963d9c`
 
   const searchLocation = (event) => {
-    if (event.key === 'Enter') {
+     
       axios.get(url).then((response) => {
         setData(response.data)
         console.log(response.data)
       })
       setLocation('')
-    }
+    
   }
 
   return (
@@ -23,9 +23,9 @@ function App() {
         <input
           value={location}
           onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
           placeholder='Enter Location'
           type="text" />
+       <button className='btn' onClick={searchLocation}>Submit</button>
       </div>
       <div className="container">
         <div className="top">
